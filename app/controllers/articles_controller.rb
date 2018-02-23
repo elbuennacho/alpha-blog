e07ddb36e0d8e6cd19ae6f79e9c1260b0d6001f9
Ignacio Velazquez <juanignaciouaem@gmail.com>
2018-02-23 01:18:35 +0000

    Update all views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -53,7 +53,7 @@ class ArticlesController < ApplicationController
   end 
   
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
     end
     
     def require_same_user
@@ -61,5 +61,5 @@ class ArticlesController < ApplicationController
         flash[:danger] = "You can only edit or delete your own articles"
         redirect_to root_path
       end
-      end
+    end
 end 
